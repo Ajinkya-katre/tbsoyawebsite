8001aa30168f5a684af1acb6e670baa2e82acdcc
Ajinkya Katre <katreajinkya@gmail.com>
2024-02-28 09:56:35 +0530

    Project created using react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {NavbarDemo} from '../src/pages/navbar'
+import { ImagesSliderDemo } from './pages/hero-page-slider';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarDemo/>
+      <ImagesSliderDemo/>
     </div>
   );
 }
